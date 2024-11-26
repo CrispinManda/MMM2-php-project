69cf5d6aed4cc8b57e33853fddece3fed285c1a6
Crispin Manda <crispinmanda06@gmail.com>
2024-11-26 19:31:18 +0300

    added the files

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'header.php' ?>
+<?php include 'nav.php' ?>
+
+
+<h1>this is h1 without bootstrap</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima recusandae, repellat incidunt porro, architecto, enim ipsa minus adipisci facere temporibus voluptas ducimus in a maxime explicabo perspiciatis ea accusamus similique.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php' ?>
